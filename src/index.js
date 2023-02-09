387a5c56ade9b1a10560b7c4e798d5990683acf5
tdronfield <tay.dronfield@gmail.com>
2023-02-09 12:33:33 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,98 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default function App() {
+    const [todos, setTodos] = React.useState([
+      { id: 1, text: "Do Dishes", done: false },
+      { id: 2, text: "Do Laundry", done: false },
+      { id: 3, text: "Do Groceries", done: false },
+    ])
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    return (
+      <div className="App">
+        <h1>To-Do List</h1>
+        
+        <ToDoList setTodos={setTodos} todos={todos} />
+        <AddToDo setTodos={setTodos} />
+      </div>
+    );
+}
+
+function ToDoList({ todos, setTodos }) {
+  function handleToggleTodo(todo) {
+    const updatedTodos = todos.map((t) => t.id === todo.id ? {
+        ...t,
+        done: !t.done
+      } : t);
+    setTodos(updatedTodos)
+  }
+
+  if(!todos.length) {
+    return <p>No to-dos left!</p>;
+  }
+
+  return (
+    <ul>
+      {todos.map(todo => (
+        <li 
+        onDoubleClick={() => handleToggleTodo(todo)}
+        style={{ textDecoration: todo.done ? 'line-through' : '' }}
+        key={todo.id}>
+          { todo.text }
+          <DeleteTodo todo={todo} setTodos={setTodos} />
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+function DeleteTodo({ todo, setTodos }) {
+  function handleDeleteTodo() {
+    const confirmed = window.confirm("Do you want to delete this?");
+    if (confirmed) {
+      setTodos((prevTodos) => {
+        return prevTodos.filter((t) => t.id !== todo.id);
+      });
+    }
+  }
+
+  return (
+    <span 
+    onClick={handleDeleteTodo}
+    role="button" 
+    style={{
+      color: 'red',
+      fontWeight: 'bold',
+      marginLeft: 10,
+    }}>X</span>
+  )
+}
+
+function AddToDo({ setTodos }) {
+  const inputRef = React.useRef();
+
+  function handleAddToDo(event) {
+    event.preventDefault();
+    const text = event.target.elements.addToDo.value;
+    const todo = {
+      id: Math.random(),
+      text,
+      done: false
+    };
+    setTodos(prevTodos => {
+      return prevTodos.concat(todo)
+    })
+    inputRef.current.value = "";
+  }
+
+  return (
+    <form onSubmit={handleAddToDo}>
+      <input name="addToDo" placeholder="Add To-Do" ref={inputRef} />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
